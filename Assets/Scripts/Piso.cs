2bee64dfa642d1fc6d0b5cce95de9b24cd172a38
Douglas Leal <douglas.leal@live.com>
2023-02-21 11:35:02 -0300

    fazendo piso infinito

---
+++ Assets/Scripts/Piso.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piso : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    private Vector3 posicaoInicial;
+    private float tamanhoDaImagem;
+    private float tamanhoRealDaImagem;
+    private float deslocamento;
+
+    private void Awake()
+    {
+        posicaoInicial = transform.position;
+        tamanhoDaImagem = GetComponent<SpriteRenderer>().size.x;
+        tamanhoRealDaImagem = tamanhoDaImagem * transform.localScale.x;
+    }
+
+    void Update()
+    {
+        deslocamento = Mathf.Repeat(speed * Time.time, tamanhoRealDaImagem);
+
+        transform.position = posicaoInicial + (Vector3.left * deslocamento);
+
+
+        //transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
