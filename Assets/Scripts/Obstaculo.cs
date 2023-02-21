a435609be8eb2512d2904eaa6661a8e19f42f7fd
Douglas Leal <douglas.leal@live.com>
2023-02-21 19:26:35 -0300

    Variando altura dos obstaculos

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -7,6 +7,14 @@ public class Obstaculo : MonoBehaviour
     [SerializeField]
     private float speed = 10f;
 
+    private float maxY = 1.9f;
+    private float minY = -1.3f;
+
+    private void Awake()
+    {
+        transform.Translate(Vector3.up * Random.Range(maxY, minY));
+    }
+
     void Update()
     {
         transform.Translate(Vector3.left * Time.deltaTime * speed);
