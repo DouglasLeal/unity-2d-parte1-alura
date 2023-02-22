9c3a9a40b386b32cf924e836cfa12bb52cf58349
Douglas Leal <douglas.leal@live.com>
2023-02-22 16:36:13 -0300

    Mostrando mensagem de GameOver

--- Assets/Scripts/Diretor.cs
+++ Assets/Scripts/Diretor.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject imagemGameOver;
+    
     public void FinalizarJogo()
     {
         Time.timeScale = 0;
+        imagemGameOver.SetActive(true);
     }
 }
