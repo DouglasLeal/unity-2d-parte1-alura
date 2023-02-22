c790400fd0601185198334e98b1eccd66032f0ca
Douglas Leal <douglas.leal@live.com>
2023-02-22 17:32:40 -0300

    Trilha sonora e som ao pontuar adicionados

--- Assets/Scripts/Pontuacao.cs
+++ Assets/Scripts/Pontuacao.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 public class Pontuacao : MonoBehaviour
 {
     private int _pontos;
+    private AudioSource _audioPontuar;
 
     [SerializeField]
     private Text _textoPontuacao;
+
+    private void Awake()
+    {
+        _audioPontuar = GetComponent<AudioSource>();
+    }
 
     public void Pontuar()
     {
         _pontos++;
         _textoPontuacao.text = _pontos.ToString();
+        _audioPontuar.Play();
     }
 
     public void Zerar()
